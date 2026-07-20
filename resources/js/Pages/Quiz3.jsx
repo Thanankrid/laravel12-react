3c5a536a4c8a135546fd7afdc1a8d33f3837710b
Thanankrid07 <thanankrid.chai@vru.ac.th>
2026-07-20 15:18:35 +0700

    Update Quiz3.jsx

--- resources/js/Pages/Quiz3.jsx
+++ resources/js/Pages/Quiz3.jsx
@@ -178,7 +178,7 @@ export default function Quiz3() {
                 }}
                 onClick={handleSubmit}
               >
-                ส่งความพึงพอใจ 🚀
+                ส่งความพึงพอใจ!🚀
               </button>
             </div>
           )}
